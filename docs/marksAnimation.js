30ea0d365e8a67e128cff139e7d0f0ddbe4dfe0b
Lance <lmreineer@gmail.com>
2022-12-23 09:57:32 +1000

    Add the marks function

--- docs/marksAnimation.js
+++ docs/marksAnimation.js
@@ -1,19 +1,44 @@
-const allBoxes = document.querySelectorAll('.text')
-const upLeftBox = document.querySelector('.up-left')
-const upMidBox = document.querySelector('.up-mid')
-const upRightBox = document.querySelector('.up-right')
-const midLeftBox = document.querySelector('.mid-left')
-const centerBox = document.querySelector('.center')
-const midRightBox = document.querySelector('.mid-right')
-const botLeftBox = document.querySelector('.bot-left')
-const botMidBox = document.querySelector('.bot-mid')
-const botRightBox = document.querySelector('.bot-right')
+const allBoxes = document.querySelectorAll('.text');
+const upLeftBox = document.querySelector('.up-left');
+const upMidBox = document.querySelector('.up-mid');
+const upRightBox = document.querySelector('.up-right');
+const midLeftBox = document.querySelector('.mid-left');
+const centerBox = document.querySelector('.center');
+const midRightBox = document.querySelector('.mid-right');
+const botLeftBox = document.querySelector('.bot-left');
+const botMidBox = document.querySelector('.bot-mid');
+const botRightBox = document.querySelector('.bot-right');
 
-upLeftBox.addEventListener('click', (e) => {
-  if(!upLeftBox.innerHTML.includes('X') && !upLeftBox.innerHTML.includes('O')) {
-    upLeftBox.innerHTML = 'X';
+let functionIsRunning = false;
+
+allBoxes.forEach((box) => {
+  box.addEventListener('click', (e) => {
+    clickFunction(e, box);
+  })
+})
+
+function clickFunction(e, box) {
+  if (!functionIsRunning) {
+    firstPlayer(e, box);
+    functionIsRunning = true;
+  } else {
+    secondPlayer(e, box);
+    functionIsRunning = false;
   }
-  else if(!upLeftBox.innerHTML.includes('X') && !upLeftBox.innerHTML.includes('O')) {
-    upLeftBox.innerHTML = 'O';
+}
+
+function firstPlayer(e, box) {
+  if (!box.innerHTML.includes('X')) {
+    if (!box.innerHTML.includes('O')) {
+      e.target.innerHTML = 'X';
+    }
   }
-});
+}
+
+function secondPlayer(e, box) {
+  if (!box.innerHTML.includes('O')) {
+    if (!box.innerHTML.includes('X')) {
+      e.target.innerHTML = 'O';
+    }
+  }
+}
