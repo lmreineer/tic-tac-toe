9e3edcb48af056ab697a0f2ce08d6ed519f88dfb
Lance <lmreineer@gmail.com>
2022-12-23 21:55:01 +1000

    Add computer choice function

--- docs/main.js
+++ docs/main.js
@@ -1,38 +1,68 @@
-const upperTexts = document.querySelectorAll('.para');
-const restartButton = document.querySelector('.restart');
-
 const allBoxes = document.querySelectorAll('.text');
+const boxArray = Array.from(allBoxes);
 
 let functionIsRunning = false;
 
 allBoxes.forEach((box) => {
-  box.addEventListener('click', (e) => {
-    clickFunction(e, box);
+  box.addEventListener('click', () => {
+    getPlayerChoice(box);
   })
 })
 
-function clickFunction(e, box) {
-  if (!functionIsRunning) {
-    firstPlayer(e, box);
-    functionIsRunning = true;
-  } else {
-    secondPlayer(e, box);
-    functionIsRunning = false;
-  }
+// after click, function getComputerChoice
+
+function getPlayerChoice(box) {
+  box.innerHTML = 'X';
 }
 
-function firstPlayer(e, box) {
-  if (!box.innerHTML.includes('X')) {
-    if (!box.innerHTML.includes('O')) {
-      e.target.innerHTML = 'X';
-    }
-  }
-}
+// function randomBox(random) {
+  // random = boxArray[Math.floor(Math.random() * boxArray.length)];
+  
+// }
 
-function secondPlayer(e, box) {
-  if (!box.innerHTML.includes('O')) {
+function getComputerChoice(box) {
+  let emptyBox = [...allBoxes].filter(box => box.innerHTML.includes('X') === false);
+  if (emptyBox.length > 0) {
+    random = allBoxes[Math.floor(Math.random() * emptyBox.length)];
     if (!box.innerHTML.includes('X')) {
-      e.target.innerHTML = 'O';
+      if (!box.innerHTML.includes('O')) {
+        random.innerHTML = 'O';
+      }
     }
   }
 }
+
+// function clickFunction(box) {
+//   if (!functionIsRunning) {
+//     getPlayerChoice(box);
+//     functionIsRunning = true;
+//   } else {
+//     functionIsRunning = false;
+//     getComputerChoice(box);
+//   }
+// }
+
+// function firstPlayer(e, box) {
+//   if (!box.innerHTML.includes('X')) {
+//     if (!box.innerHTML.includes('O')) {
+//       e.target.innerHTML = 'X';
+//     }
+//   }
+// }
+
+// function secondPlayer(e, box) {
+//   if (!box.innerHTML.includes('O')) {
+//     if (!box.innerHTML.includes('X')) {
+//       e.target.innerHTML = 'O';
+//     }
+//   }
+// }
+
+const restartButton = document.querySelector('.restart-button');
+
+restartButton.addEventListener('click', () => {
+  allBoxes.forEach((box) => {
+    box.innerHTML = '';
+    functionIsRunning = false;
+  })
+})
