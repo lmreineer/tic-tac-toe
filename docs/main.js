665553836fbedbc0d3cd36100bcdf6acee8f6af7
Lance <lmreineer@gmail.com>
2022-12-28 16:49:20 +1000

    Computer function done

--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,4 @@
 const allBoxes = document.querySelectorAll('.text');
-const boxTexts = document.querySelectorAll('.text');
 const upLeftBox = document.querySelector('.up-left');
 const upMidBox = document.querySelector('.up-mid');
 const upRightBox = document.querySelector('.up-right');
@@ -9,35 +8,6 @@ const midRightBox = document.querySelector('.mid-right');
 const bottomLeftBox = document.querySelector('.bot-left');
 const bottomMidBox = document.querySelector('.bot-mid');
 const bottomRightBox = document.querySelector('.bot-right');
-
-let functionIsRunning = false;
-
-allBoxes.forEach((box) => {
-  box.addEventListener('click', () => {
-    getPlayerChoice(box);
-    checkWhosWinner();
-  })
-  
-  box.addEventListener('mousedown', () => {
-    setActiveClass(box);
-  })
-})
-
-function getPlayerChoice(box) {
-  if (!box.innerHTML.includes('X')) {
-    if (!box.innerHTML.includes('O')) {
-      box.innerHTML = 'X';
-      getComputerChoice(box);
-    }
-  }
-  anime({
-    targets: '.active',
-    scale: [2,1],
-    opacity: [0,1],
-    easing: "easeOutCirc",
-    duration: 800,
-  })
-}
 
 // non-functioning bug
 // this function knows specific boxes
@@ -54,51 +24,78 @@ function getPlayerChoice(box) {
 // computer actually looks like it's trying to win sometimes.
 
 // issue, not animating unless you double click it, also take note of higher chances of computer picking a box
-function getComputerChoice() {
+
+function animateO() {
+  anime({
+    targets: '.oActive',
+    scale: [1.2, 1],
+    easing: 'easeOutCirc',
+    duration: 200,
+  });
+}
+
+function markX(box) {
+  if (!box.innerHTML.includes('X')
+  && !box.innerHTML.includes('O')) {
+    box.innerHTML = 'X';
+    markO();
+  }
+}
+
+function markO() {
   setTimeout(() => {
-    let emptyBox = [...allBoxes].filter(box => box.innerHTML.trim() === '');
-    let random = allBoxes[Math.floor(Math.random() * emptyBox.length)];
-    console.log(emptyBox)
-    if (!random.innerHTML.includes('X') &&
-    !random.innerHTML.includes('O')) {
-      random.innerHTML = 'O';
+    const restAllBoxes = [...allBoxes].filter((box) => !box.innerHTML.includes('X') && !box.innerHTML.includes('O'));
+    const randomBox = rando(restAllBoxes).value;
+    randomBox.innerHTML = 'O';
+
+    // different function but the same purpose as xActive, this instead replaces 'text' class
+    const clickedBox = randomBox;
+    for (let i = 0; i < allBoxes.length; i++) {
+      allBoxes[i].classList.replace('oActive', 'text');
     }
-    setComputerActiveClass(random);
+    clickedBox.classList.replace('text', 'oActive');
+    animateO();
   }, 500);
 }
 
+// function markX(box) {
+//   if (!box.innerHTML.includes('X')) {
+//     if (!box.innerHTML.includes('O')) {
+//       box.innerHTML = 'X';
+//       markO(box);
+//     }
+//   }
+// }
 
-function setActiveClass(box) {
-  for (let i = 0; i < [...allBoxes].length; i++) {
-    let clicked = boxTexts[i];
-    clicked.addEventListener('click', () => {
-      let active = document.querySelector('.text.active');
-      if (active) {
-        active.classList.remove('active');
-      }
-      box.classList.add('active');
-    })
-  }
+function animateX() {
+  anime({
+    targets: '.xActive',
+    scale: [1.2, 1],
+    easing: 'easeOutCirc',
+    duration: 200,
+  });
 }
 
-function setComputerActiveClass(random) {
-  for (let i = 0; i < [...allBoxes].length; i++) {
-    let computerClicked = boxTexts[i];
-    computerClicked.addEventListener('click', () => {
-      let computerActive = document.querySelector('.text.computerActive');
-      if (computerActive) {
-        computerActive.classList.remove('computerActive');
-      }
-      random.classList.add('computerActive');
-    })
-  }
-}
+// to add class to clicked box then remove the previous box's class to add to new box if new is clicked
+allBoxes.forEach((btnClickEvent, _, buttons) => {
+  btnClickEvent.addEventListener('click', () => {
+    buttons.forEach((bt) => {
+      bt.classList.toggle('xActive', bt === btnClickEvent);
+    });
+  });
+
+  btnClickEvent.addEventListener('click', () => {
+    if (!btnClickEvent.innerHTML.includes('X')) {
+      animateX();
+    }
+  });
+});
 
 function checkWhosWinner() {
-  if (bottomRightBox.innerHTML.includes('X') &&
-  bottomMidBox.innerHTML.includes('X') &&
-  bottomLeftBox.innerHTML.includes('X')) {
-    centerBox.innerHTML = 'X win'
+  if (bottomRightBox.innerHTML.includes('X')
+  && bottomMidBox.innerHTML.includes('X')
+  && bottomLeftBox.innerHTML.includes('X')) {
+    centerBox.innerHTML = 'X win';
   }
 }
 
@@ -128,11 +125,17 @@ function checkWhosWinner() {
 //   }
 // }
 
+allBoxes.forEach((box) => {
+  box.addEventListener('click', () => {
+    markX(box);
+    checkWhosWinner();
+  });
+});
+
 const restartButton = document.querySelector('.restart-button');
 
 restartButton.addEventListener('click', () => {
   allBoxes.forEach((box) => {
     box.innerHTML = '';
-    functionIsRunning = false;
-  })
-})
+  });
+});
